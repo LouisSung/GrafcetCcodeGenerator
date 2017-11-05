7ebe2cdfeaeed415caaf6cf99c48b3e1ec9a4242
LouisSung <LouisSung@LS-MBPR.local>
2017-11-05 22:22:51 +0800

    Initial Commit

---
+++ generateGrafcetCcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  generateGrafcetCcode
+//
+//  Created by LouisSung on 2017/11/5.
+//  Copyright © 2017年 LS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
