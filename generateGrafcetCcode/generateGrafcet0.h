bfa36c71e955c4a3f1da8c56f29515787a637a29
LouisSung <LouisSung@LS-MBPR.local>
2017-11-05 22:39:39 +0800

    完成generateGrafcet0()

---
+++ generateGrafcetCcode/generateGrafcet0.h
@@ -0,0 +1,14 @@
+//
+//  generateGrafcet0.h
+//  generateGrafcetCcode
+//
+//  Created by LouisSung on 2017/11/1.
+//  Copyright © 2017年 LS. All rights reserved.
+//
+
+#ifndef drawGrafcet0_h
+#define drawGrafcet0_h
+
+#include <stdio.h>
+void generateGrafcet0(void) ;
+#endif /* generateGrafcet0_h */
--- generateGrafcetCcode/main.c
+++ generateGrafcetCcode/main.c
@@ -2,14 +2,23 @@
 //  main.c
 //  generateGrafcetCcode
 //
-//  Created by LouisSung on 2017/11/5.
+//  Created by LouisSung on 2017/11/1.
 //  Copyright © 2017年 LS. All rights reserved.
 //
 
 #include <stdio.h>
+#include "generateGrafcet0.h"
+//#include "generateSubGrafcet.h"
+const char* EOL="\r\n" ;
+const char outputPath[]="" ;
+char grafcetID[5+1] ;			//_Grafcet最深層數(5層)+1
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-	return 0;
+	printf("要製作的Grafcet編號(0 | [1-9A-Z][0-9A-Z]*)： ") ;
+	scanf("%s", grafcetID) ;
+	if(grafcetID[0]=='0'){
+		generateGrafcet0() ;}
+//	else{
+//		generateSubGrafcet() ;}
+	return 0 ;
 }
