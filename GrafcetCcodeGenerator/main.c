6447f52e39be0e923ce4e2ae9c8ea70905e4989f
LouisSung <LouisSung@LS-MBPR.local>
2017-11-09 20:45:36 +0800

    修改RE ฅ• ω •ฅ

--- GrafcetCcodeGenerator/main.c
+++ GrafcetCcodeGenerator/main.c
@@ -14,7 +14,7 @@ const char outputPath[]="" ;
 char grafcetID[5+1] ;			//_Grafcet最深層數(5層)+1
 
 int main(int argc, const char * argv[]) {
-	printf("要製作的Grafcet編號(0 | [1-9A-Z][0-9A-Z]*)： ") ;
+	printf("要製作的Grafcet編號(0 | [1-9A-Z]+)： ") ;
 	scanf("%s", grafcetID) ;
 	if(grafcetID[0]=='0'){
 		generateGrafcet0() ;}
